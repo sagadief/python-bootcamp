bdd4de8f30ff4470fa4159bb91d1b1b11c7f0680
Thrace Bebe <thracebe@va-b5.kzn.21-school.ru>
2023-05-26 21:32:38 +0300

    add ex1

--- tasks/day_02/00_Gaining_Access.py
+++ tasks/day_02/00_Gaining_Access.py
@@ -1,1 +1,25 @@
-def 
+class Key:
+    passphrase = "zax2rulez"
+
+    # def __init__(self):
+    #     pass
+      
+    def __len__(self):
+        return 1337
+
+    def __getitem__(self, value):
+        return 3 if value == 404 else "err"
+
+    def __str__(self) -> str:
+        return "GeneralTsoKeycard"
+
+    def __gt__(self, value):
+        return value > 8999
+
+if __name__ == "__main__":
+    key = Key()
+    print(len(key))
+    print(key[404])
+    print(key > 9000)
+    print(key.passphrase)
+    print(str(key))
